1f19de41d7563b7b86cc686c23f9bd082a1257c1
zainab <zi4370106@gmail.com>
2024-06-21 14:25:53 +0500

    youtube image change

--- TPT.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    const menu = document.getElementById('mobile-menu');
-    menu.classList.toggle('hidden');
-}
-document.getElementById('menu-icon').addEventListener('click', function() {
-    var menu = document.getElementById('mobile-menu');
-    menu.classList.toggle('hidden');
-});
-
---
+++ index.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const menu = document.getElementById('mobile-menu');
+    menu.classList.toggle('hidden');
+}
+document.getElementById('menu-icon').addEventListener('click', function() {
+    var menu = document.getElementById('mobile-menu');
+    menu.classList.toggle('hidden');
+});
