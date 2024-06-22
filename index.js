b8e9ef7be3fd95cfd4b4274da3e1223860e37ada
zainab <zi4370106@gmail.com>
2024-06-22 11:39:04 +0500

    all images include remove lazy loading

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-icon').addEventListener('click', function() {
+    const mobileMenu = document.getElementById('mobile-menu');
+    mobileMenu.classList.toggle('hidden');
+});
