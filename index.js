04d2c692fd6e163ea5699a8ad1fe73f246a7f7e9
zainab <zi4370106@gmail.com>
2024-06-28 11:40:50 +0500

    add css file and do some changes

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ const mobileMenu = document.getElementById('mobile-menu');
 menuIcon.addEventListener('click', function() {
   mobileMenu.style.display = mobileMenu.style.display === 'none' ? 'block' : 'none';
 });
+
